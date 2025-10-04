74622cef778d7e443a34422736211847820e3bcd
Tomer Galansky <tomerg91@gmail.com>
2025-10-04 22:08:29 +0300

    Guard server env without server-only directive

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,5 +1,7 @@
-import 'server-only';
-
 import { env, type ServerEnv } from './base';
+
+if (typeof window !== 'undefined') {
+  throw new Error('serverEnv can only be used in a server-side context');
+}
 
 export const serverEnv: ServerEnv = env.server;
